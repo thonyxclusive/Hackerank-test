47b06bf2a9525bba744bc809f24f27aa0af05b84
thonyxclusive <123905984+thonyxclusive@users.noreply.github.com>
2024-06-18 17:52:10 +0100

    integer and float division

---
+++ division/division.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    myint_division = a//b
+    myfloat_division = a/b
+    print (myint_division)
+    print (myfloat_division)
