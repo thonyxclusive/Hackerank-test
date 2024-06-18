d14744bae348589e1ee4bb52027e842e77f839cc
thonyxclusive <123905984+thonyxclusive@users.noreply.github.com>
2024-06-18 10:02:23 +0100

    Addition, subtraction and product of two numbers

---
+++ arithmetic-operators/arithmetic-operators.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    the_sum = a + b
+    the_diff = a - b
+    the_product = a * b
+    print (the_sum)
+    print (the_diff)
+    print (the_product)
+     
